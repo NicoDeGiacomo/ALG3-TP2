a5440e2fe00847bf043fe2d73947d95cdaf9dad9
Nicolas De Giacomo <nicolas.de.giacomo@gmail.com>
2018-11-02 15:54:22 -0300

    Minor Fix Tests

--- test/ejemplo/FooTests.java
+++ test/ejemplo/FooTests.java
@@ -1,24 +1,22 @@
 package ejemplo;
 
-import junit.framework.Assert;
+import org.junit.Assert;
 import org.junit.Test;
 
-public class FooTest {
+public class FooTests {
 
-	@Test
-	public void doFooShouldReturnFoo(){
+    @Test
+    public void doFooShouldReturnFoo(){
         Foo foo = new Foo();
         String result = foo.doFoo();
         Assert.assertEquals("Foo", result);
-	}
-	
-	/*
+    }
+
 	@Test
 	public void doBarShouldReturnBar(){
         Foo foo = new Foo();
         String result = foo.doBar();
         Assert.assertEquals("Bar", result);
 	}
-	*/
 
 }
