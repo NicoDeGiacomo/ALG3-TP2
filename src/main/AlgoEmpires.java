c2d727106bc0275b79f699af5c34de84e69303b4
Nico De Giacomo <nicolas.de.giacomo@gmail.com>
2018-11-05 21:54:14 -0300

    Removed main from AlgoEmpires

--- src/main/AlgoEmpires.java
+++ src/main/AlgoEmpires.java
@@ -1,10 +1,6 @@
 package main;
 
 public class AlgoEmpires {
-
-	public static void main(String[] args) {
-		System.out.println("AlgoEmpires bye");
-	}
 	
 	String doFoo() {
 		return "AlgoEmpires";
