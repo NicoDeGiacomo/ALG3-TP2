e14f3e6e55e7aa478a6790c19c2c3949f24d80a0
Nicolas De Giacomo <nicolas.de.giacomo@gmail.com>
2018-11-27 13:45:27 -0300

    Minor fix

--- src/ui/Pantalla.java
+++ src/ui/Pantalla.java
@@ -11,7 +11,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
